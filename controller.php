f6bcb8954bb8f5c575910495638f7d09df3cd100
Marco Kuoni <marco@lemonbrain.ch>
2019-05-04 20:00:30 +0200

    oops forgot parent::uninstall

--- controller.php
+++ controller.php
@@ -63,6 +63,8 @@ class Controller extends Package
 
         $this->removeSinglePage('/dashboard/system/environment/graphql');
         SchemaBuilder::deleteSchemaFile();
+
+        parent::uninstall();
     }
     
     private function addSinglePages(){
