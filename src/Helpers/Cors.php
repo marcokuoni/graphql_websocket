216e8b732370cdbad451944b40c93cdc1966fb45
lemonbrain Marco <marco@lemonbrain.ch>
2022-03-25 15:33:08 +0100

    cors to helper

--- src/Helpers/Cors.php
+++ src/Helpers/Cors.php
@@ -15,13 +15,13 @@ class Cors
         if (in_array($_SERVER['HTTP_ORIGIN'], $origins)) {
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 600');
-            header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN']);
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-                header('Access-Control-Allow-Headers', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
             } else {
-                header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+                header('Access-Control-Allow-Headers: Authorization, Content-Type');
             }
-            header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+            header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
         }
     }
 }
